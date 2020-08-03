96910f252a339be3e31a6292338c99d68a527d83
kwlo <kwlo@github.com>
2020-08-03 01:22:01 +0000

    Remove unused packages

--- pack/level2/level2.go
+++
@@ -1,9 +0,0 @@
-package level2
-
-import (
-	"go.uber.org/zap"
-)
-
-func Test3() {
-	zap.S().Infof("Running level2.go")
-}
--- pack/pack.go
+++
@@ -1,9 +0,0 @@
-package pack
-
-import (
-	"go.uber.org/zap"
-)
-
-func Test() {
-	zap.S().Infof("Running test.go")
-}
