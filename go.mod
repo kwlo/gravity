2027b3da9e1695122e76210c1b04f540c9d2b0cd
kwlo <kwlo@github.com>
2020-08-04 18:33:23 +0000

    Refactor test case to table driven

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kwlo/gravity
 
 go 1.14
 
-require go.uber.org/zap v1.15.0
+require (
+	go.uber.org/zap v1.15.0
+	golang.org/x/tools/gopls v0.4.4 // indirect
+)
--- physics/utils_test.go
+++ physics/utils_test.go
@@ -5,31 +5,30 @@ import (
 	"testing"
 )
 
-const float64EqualityThreshold = 1e-6
+const float64EqualityThreshold = 1e-9
 
 func TestConvertDistance(t *testing.T) {
-	if ConvertDistance(10, Yards, Inches) != 360 {
-		t.Errorf("wrong result")
+	tests := []struct {
+		num    float64
+		source DistanceEnum
+		target DistanceEnum
+		want   float64
+	}{
+		{10, Yards, Inches, 360},
+		{72, Inches, Yards, 2},
+		{3.5, Normalized, Yards, 35000},
+		{-3.5, Normalized, Yards, -35000},
+		{827, Yards, Normalized, 0.0827},
+		{958, Yards, Miles, 0.544318182},
+		{6.41, Miles, Yards, 11281.6},
+		{5.21, Normalized, Inches, 1875600},
+		{71, Inches, Normalized, 0.000197222},
 	}
-	if ConvertDistance(72, Inches, Yards) != 2 {
-		t.Errorf("wrong result")
-	}
-	if ConvertDistance(3.5, Normalized, Yards) != 35000 {
-		t.Errorf("wrong result")
-	}
-	if math.Abs(ConvertDistance(827, Yards, Normalized)-0.0827) > float64EqualityThreshold {
-		t.Errorf("wrong result")
-	}
-	if math.Abs(ConvertDistance(958, Yards, Miles)-0.544318) > float64EqualityThreshold {
-		t.Errorf("wrong result")
-	}
-	if math.Abs(ConvertDistance(6.41, Miles, Yards)-11281.6) > float64EqualityThreshold {
-		t.Errorf("wrong result")
-	}
-	if math.Abs(ConvertDistance(5.21, Normalized, Inches)-1875600) > float64EqualityThreshold {
-		t.Errorf("wrong result")
-	}
-	if math.Abs(ConvertDistance(71, Inches, Normalized)-0.000197222) > float64EqualityThreshold {
-		t.Errorf("wrong result")
+
+	for idx, tc := range tests {
+		got := ConvertDistance(tc.num, tc.source, tc.target)
+		if math.Abs(got-tc.want) > float64EqualityThreshold {
+			t.Fatalf("expected for case #%d %v, %v, got: %v", idx, tc.num, tc.want, got)
+		}
 	}
 }
