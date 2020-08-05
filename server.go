dd6a3c0e2a47732a068d34986b0d16a64ad9b981
kwlo <kwlo@github.com>
2020-08-05 02:51:56 +0000

    Added healthcheck

--- server.go
+++ server.go
@@ -13,13 +13,13 @@ type Server struct {
 // Route Sample route. To be deleted
 func (srv *Server) Route(w http.ResponseWriter, r *http.Request) {
 	srv.logger.Infof("Served: %s", r.URL.Path[1:])
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "pong")
 }
 
 // Start starts up server and listens at port
 func (srv *Server) Start(port int) {
 	srv.logger.Infof("Starting Server at port %d", port)
 
-	http.HandleFunc("/", srv.Route)
+	http.HandleFunc("/ping", srv.Route)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
